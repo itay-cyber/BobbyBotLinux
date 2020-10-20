abde37e50bcb22082cff8a57d0b64a6e59ab281f
itay-cyber <itaykag@gmail.com>
2020-10-20 20:47:44 +0200

    Made bobby bot online: to fix
    added main.py

--- server.py
+++ server.py
@@ -1,15 +1,22 @@
-
+import os
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
 class Serv(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        print(self.path)
         if self.path == '/':
             self.path = 'web/index.html'
+
+
+            stuff = os.listdir("web/")
+            print(stuff)
+
         try:
      
-            file_to_open = open(self.path[1:]).read()
+            file_to_open = open(self.path).read()
+            print("File to open: {}".format(file_to_open))
             self.send_response(200)
         except:
             file_to_open = "File not found"
@@ -17,8 +24,11 @@ class Serv(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes(file_to_open, 'utf-8'))
 
+
 def run():
-    httpd = HTTPServer(('localhost', 5500), Serv)
+    httpd = HTTPServer(('localhost', 6900), Serv)
     httpd.serve_forever()
 
+
+run()
 
